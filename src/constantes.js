ba964dbabbe9763b4aa7e26fdef55b1abd706946
fernandovs4 <fervsantos3000@gmail.com>
2024-05-06 05:16:30 -0300

    feito

--- src/constantes.js
+++
@@ -1,5 +0,0 @@
-const host = "http://3.140.128.237"
-const apialuno = "https://aluno.api.projetohorizontes.com"
-const api_recomendacao = "https://recomendacao.api.projetohorizontes.com"
-
-export {apialuno, api_recomendacao}
